8d74f0c04f1e7ef5c10a79dc3b0626056bad5c96
piyushgaud <46211842+piyushgaud@users.noreply.github.com>
2026-09-22 13:20:34 +0530

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.java
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if(intervals.size()==0) return true;
+
+        intervals.sort((a,b)->Integer.compare(a.start,b.start));
+
+        Interval interval = intervals.get(0);
+        for(int i=1;i<intervals.size();i++)
+        {
+            if(interval.end>intervals.get(i).start)
+            {
+                return false;
+            }
+            interval = intervals.get(i);
+        }
+        return true;
+    }
+}
